b9377c2e32bf87d247c0480c6ea25b6aad45eb66
rxagar10 <rxagar10@gmail.com>
2021-07-30 22:59:00 -0400

    finished CreateRec Page

--- src/components/CreateRec.js
+++ src/components/CreateRec.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import InfoPopup from "./misc-components/InfoPopup";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
 import axios from 'axios';
 import config from "../config";
+import {useHistory} from "react-router-dom";
 
 function CreateRec(props) {
 
+  // const [movie, setMovie] = useState({
+  //   genres: "",
+  //
+  // })
+  // setMovie({
+  //   ...movie, genres: "sad"
+  // })
   const [errorMessage, setErrorMessage] = useState("");
   const [myFriends, setMyFriends] = useState([]);
   const [friendsList, setFriendsList] = useState([]);
@@ -17,13 +25,23 @@ function CreateRec(props) {
   const [title, setTitle] = useState("");
   const [titlesList, setTitlesList] = useState([]);
 
-  const [genres, setGenres] = useState([]);
+  const [genres, setGenres] = useState("");
   const [overview, setOverview] = useState("");
   const [runtime, setRuntime] = useState("");
   const [year, setYear] = useState("");
   const [numberOfSeasons, setNumberOfSeasons] = useState("");
   const [numberOfEpisodes, setNumberOfEpisodes] = useState("");
   const [comments, setComments] = useState("");
+
+  let history = useHistory()
+
+  useEffect(() => {
+    axios.post(config.host + config.port + "/createRec", {
+      username: props.username
+    }).then((resp) => {
+      setMyFriends(resp.data.myFriends)
+    })
+  }, [])
 
   const makeTitle = () => {
     if (recType === "") {
@@ -32,12 +50,24 @@ function CreateRec(props) {
       return (
           <div>
             <label htmlFor="title">Title</label>
-            <input list="titles" name="title" onChange={(e) => setTempTitle(e.target.value)}/>
+            <input list="titles" name="title" onChange={(e) => {
+              setTempTitle(e.target.value)
+              const selectedTitle = titlesList.find((title) => (title.title === e.target.value))
+              if(selectedTitle !== undefined) {
+                setTitle(e.target.value)
+                setGenres(selectedTitle.genres)
+                setOverview(selectedTitle.overview)
+                setRuntime(selectedTitle.runtime)
+                setYear(selectedTitle.year)
+                setNumberOfEpisodes(selectedTitle.numberOfEpisodes)
+                setNumberOfSeasons(selectedTitle.numberOfSeasons)
+              }
+            }} value={tempTitle}/>
 
             <datalist id="titles">
               {
                 titlesList.map(title => {
-                  return <option value={title.title + " (" + title.year + ")"} onClick={() => setTitle(title.title)} />
+                  return <option label={" (" + title.year + ")"} value={title.title} />
                 })
               }
             </datalist>
@@ -54,6 +84,7 @@ function CreateRec(props) {
     })
     .then((resp) => {
       setTitlesList(resp.data.titlesList)
+
     })
   }
 
@@ -68,19 +99,29 @@ function CreateRec(props) {
       return (
           <div className="movieForm">
             <label htmlFor="movieGenre">Genre</label>
-            <input type="text" name="movieGenre" />
+            <input type="text" name="movieGenre" onChange={(e) => {
+              setGenres(e.target.value)
+            }} value={genres}/>
 
             <label htmlFor="movieOverview">Overview</label>
-            <input type="text" name="movieOverview" />
+            <input type="text" name="movieOverview" onChange={(e) => {
+              setOverview(e.target.value)
+            }} value={overview}/>
 
             <label htmlFor="movieRuntime">Runtime</label>
-            <input type="text" name="movieRuntime" />
+            <input type="text" name="movieRuntime" onChange={(e) => {
+              setRuntime(e.target.value)
+            }} value={runtime}/>
 
             <label htmlFor="movieYear">Year of Release</label>
-            <input type="text" name="movieYear" />
+            <input type="text" name="movieYear" onChange={(e) => {
+              setYear(e.target.value)
+            }} value={year}/>
 
             <label htmlFor="comments">Comments</label>
-            <input type="text" name="comments" />
+            <input type="text" name="comments" onChange={(e) => {
+              setComments(e.target.value)
+            }} value={comments}/>
 
             <button className="submitRec" onClick={() => submitRec()}>Send Rec</button>
           </div>
@@ -90,19 +131,29 @@ function CreateRec(props) {
           <div className="tvShowForm">
 
             <label htmlFor="tvEpisodeRunTime">Episode Run Time</label>
-            <input type="text" name="tvEpisodeRunTime" />
+            <input type="text" name="tvEpisodeRunTime" onChange={(e) => {
+              setRuntime(e.target.value)
+            }} value={runtime}/>
 
             <label htmlFor="tvNumberOfSeasons">Number of Seasons</label>
-            <input type="text" name="tvNumberOfSeasons" />
+            <input type="text" name="tvNumberOfSeasons" onChange={(e) => {
+              setNumberOfSeasons(e.target.value)
+            }} value={numberOfSeasons}/>
 
             <label htmlFor="tvNumberOfEpisodes">Number Of Episodes</label>
-            <input type="text" name="tvNumberOfEpisodes" />
+            <input type="text" name="tvNumberOfEpisodes" onChange={(e) => {
+              setNumberOfEpisodes(e.target.value)
+            }} value={numberOfEpisodes}/>
 
             <label htmlFor="tvGenres">Genres</label>
-            <input type="text" name="tvGenre" />
+            <input type="text" name="tvGenre" onChange={(e) => {
+              setGenres(e.target.value)
+            }} value={genres}/>
 
             <label htmlFor="comments">Comments</label>
-            <input type="text" name="comments" />
+            <input type="text" name="comments" onChange={(e) => {
+              setComments(e.target.value)
+            }} value={comments}/>
 
             <button className="submitRec" onClick={() => submitRec()}>Send Rec</button>
 
@@ -139,7 +190,9 @@ function CreateRec(props) {
         friendsList,
       })
       .then((resp) => {
-
+        if(resp.data.message === 'success') {
+          history.push("/")
+        }
       })
     }
   }
@@ -172,25 +225,60 @@ function CreateRec(props) {
         />
 
         <label htmlFor="Movie">Movie</label>
-        <input type="radio" name="recOption" id="Movie" value="Movie" onClick={() => setRecType("Movie")}/>
+        <input type="radio" name="recOption" id="Movie" value="Movie" onClick={() => {
+          setRecType("Movie")
+          setTitle("")
+          setTitlesList([])
+          setTempTitle("")
+        } }/>
 
         <label htmlFor="TVShow">TV Show</label>
-        <input type="radio" name="recOption" id="TVShow" value="TVShow" onClick={() => setRecType("TVShow")}/>
+        <input type="radio" name="recOption" id="TVShow" value="TVShow" onClick={() => {
+          setRecType("TVShow")
+          setTitle("")
+          setTitlesList([])
+          setTempTitle("")
+        }}/>
 
         <label htmlFor="Music">Music</label>
-        <input type="radio" name="recOption" id="Music" value="Music" onClick={() => setRecType("Music")}/>
+        <input type="radio" name="recOption" id="Music" value="Music" onClick={() => {
+          setRecType("Music")
+          setTitle("")
+          setTitlesList([])
+          setTempTitle("")
+        }}/>
 
         <label htmlFor="Book">Book</label>
-        <input type="radio" name="recOption" id="Book" value="Book" onClick={() => setRecType("Book")}/>
+        <input type="radio" name="recOption" id="Book" value="Book" onClick={() => {
+          setRecType("Book")
+          setTitle("")
+          setTitlesList([])
+          setTempTitle("")
+        }}/>
 
         <label htmlFor="Restaurant">Restaurant</label>
-        <input type="radio" name="recOption" id="Restaurant" value="Restaurant" onClick={() => setRecType("Restaurant")}/>
+        <input type="radio" name="recOption" id="Restaurant" value="Restaurant" onClick={() => {
+          setRecType("Restaurant")
+          setTitle("")
+          setTitlesList([])
+          setTempTitle("")
+        }}/>
 
         <label htmlFor="Games">Game</label>
-        <input type="radio" name="recOption" id="Game" value="Game" onClick={() => setRecType("Game")}/>
+        <input type="radio" name="recOption" id="Game" value="Game" onClick={() => {
+          setRecType("Game")
+          setTitle("")
+          setTitlesList([])
+          setTempTitle("")
+        }}/>
 
         <label htmlFor="Other">Other</label>
-        <input type="radio" name="recOption" id="Other" value="Other" onClick={() => setRecType("Other")}/>
+        <input type="radio" name="recOption" id="Other" value="Other" onClick={() => {
+          setRecType("Other")
+          setTitle("")
+          setTitlesList([])
+          setTempTitle("")
+        }}/>
 
         {
           makeTitle()
